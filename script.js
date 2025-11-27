21dd92b921c56bb51c8a495337065d64b350a8ee
Yuval Goldberg <goldberg2003.dev@gmail.com>
2025-11-27 16:51:57 +0200

    adding fucntion to get the computer's choice: getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Working...');
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    let compChoice = Math.floor(Math.random() * 3);
+
+    console.log(compChoice);
+    return choices[compChoice];
+}
+
+console.log(getComputerChoice());
